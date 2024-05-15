1c4432acb2c6a870828beefd7cc00e20f7330bb9
PauloHenriqueJ <ph.jesus@gmail.com>
2024-05-14 22:16:53 -0300

    ContainerSubMenu

--- components/header/submenu/index.tsx
+++ components/header/submenu/index.tsx
@@ -1,5 +1,8 @@
+import { Container } from "@/components/container";
+
 export function SubMenu (){
     return (
+        <Container>
         <div className="flex w-full h-10 mx-8 my-5">
             
                 <ul className="flex  text-white gap-5  items-center justify-center ">
@@ -14,5 +17,6 @@ export function SubMenu (){
                 <div className="border-b mb-1"></div>
             
         </div>
+        </Container>
     )
 }
